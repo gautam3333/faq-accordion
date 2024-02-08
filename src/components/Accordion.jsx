a749313166563bf9f0904b8c8d2179cf9bfca962
Gautam Sarkar <gautam.sarkar3333@gmail.com>
2024-02-08 19:00:03 +0530

    fix toggle functionality

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react';
+export default function Accordion({
+  question,
+  index,
+  curIsOpen,
+  onSetCurIsOpen,
+}) {
+  const isActive = index === curIsOpen;
 
-export default function Accordion({ question }) {
-  const [isActive, setIsActive] = useState(false);
-
+  function handleToggle() {
+    onSetCurIsOpen(index);
+    if (isActive) {
+      onSetCurIsOpen(null);
+    }
+  }
   return (
     <article>
       <div className='flex items-center justify-between'>
         <h2 className='font-semibold text-darkPurple max-w-52 sm:max-w-2xl hover:cursor-pointer hover:text-purple hover:transition-all'>
           {question.question}
         </h2>
-        <button onClick={() => setIsActive((t) => !t)}>
+        <button onClick={handleToggle}>
           {isActive ? (
             <>
               <img src='/images/icon-minus.svg' alt='icon minus' />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 import Accordion from './Accordion';
 
 import { questions } from '../data/questions';
+import { useState } from 'react';
 
 export default function Card() {
+  const [curIsOpen, setCurIsOpen] = useState(null);
+
   return (
     <section className='bg-white max-w-80 rounded-lg p-6 mx-auto -mt-20 relative z-10 sm:max-w-xl shadow-md'>
       <div className='flex items-center gap-4 mb-8'>
@@ -10,12 +13,17 @@ export default function Card() {
         <h1 className='text-2xl font-bold text-darkPurple'>FAQs</h1>
       </div>
       <ul className='flex flex-col gap-4'>
-        {questions.map((query) => (
+        {questions.map((query, index) => (
           <li
             key={query.question}
             className='[&:not(:last-child)]:border-b border-lightPink pb-6'
           >
-            <Accordion question={query} />
+            <Accordion
+              question={query}
+              index={index}
+              curIsOpen={curIsOpen}
+              onSetCurIsOpen={setCurIsOpen}
+            />
           </li>
         ))}
       </ul>
